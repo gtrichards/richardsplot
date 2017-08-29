e6a122c8530f17f7d7d97528259b5c6b3d46d752
gtrichards <gtr@physics.drexel.edu>
2017-08-29 11:31:58 -0400

    Added first test plot, updated README

---
+++ testplot1.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import palettable
+import richardsplot as rplot
+
+x = np.random.random(size=1000)*6000+3000
+y = np.random.normal(loc=0,scale=0.25,size=1000)
+
+ind = np.argsort(x)
+xplot = x[ind]
+yplot = y[ind]
+
+plt.figure(figsize=(8,8))
+plt.xlim(3000,9000)
+plt.ylim(-1,1)
+plt.xlabel("Wavelength (\mu m)")
+plt.ylabel("Flux Density")
+plt.plot(xplot,yplot,color=rplot.csdark[2],label="Test1")
+plt.show()
+#plt.savefig("testplot1.pdf")
